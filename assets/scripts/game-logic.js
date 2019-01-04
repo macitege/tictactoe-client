601a2698cd0b7d79777163bb922b811323a91db3
macitege <macitegeercan@gmail.com>
2019-01-04 15:06:44 -0500

    Add Logic for when Game is Over

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,8 +1,12 @@
-const cells = ['', '', '', '', '', '', '', '', '', '']
+// Array  taht represents cells on the game board
+const cells = ['', '', '', '', '', '', '', '', '']
+// Variable that holds last players info, so that engine can check who was the last player
 let lastPlayer = null
-
+// Function that allows user to make move
 const makeMove = function (event) {
+  // Played cell's ID
   const id = event.target.id
+  // Condition to check if the box is empty and who wast the last player?
   if (cells[id] !== '') {
     alert('play on another cell')
   } else if (lastPlayer === 'o' || lastPlayer === null) {
@@ -13,6 +17,14 @@ const makeMove = function (event) {
     cells[id] = 'o'
     lastPlayer = 'o'
     $('#' + id).text('o')
+  }
+  isOver()
+}
+
+// Function to check if the game is over?
+const isOver = function () {
+  if (!cells.includes('')) {
+    alert('game is over')
   }
 }
 
